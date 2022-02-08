776d48deecae4948eb373e38b3948f93fb5258c6
Vitoria Borba <vitoriaborba@icloud.com>
2022-02-08 19:23:59 +0000

    done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (request, response, next) => {
     response.sendFile(__dirname + '/public/views/home.html');
   });
