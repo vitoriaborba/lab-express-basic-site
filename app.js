00dde4b37ec775fe9d4e5d18af421d639ca2f188
Vitoria Borba <vitoriaborba@icloud.com>
2022-02-08 16:29:23 +0000

    structure

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + '/public/views/home.html');
+  });
+  app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/public/views/about.html');
+  });
+  app.get('/works', (request, response, next) => {
+    response.sendFile(__dirname + '/public/views/works.html');
+  });
+  app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/public/views/gallery.html');
+  });
+
+  app.listen(3000)
